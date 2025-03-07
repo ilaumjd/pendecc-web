c86869e23bbefd218a81c47a89cf0d009fa447f2
ilaumjd <ilaumjd@gmail.com>
2025-03-07 16:18:19 +0700

    open link

--- src/app/success/[short_url]/page.js
+++ src/app/success/[short_url]/page.js
@@ -3,30 +3,38 @@ import { ThemeProvider } from "@emotion/react";
 import { theme } from "../../theme";
 import { Box, Button, TextField } from "@mui/material";
 import { use } from "react";
+import { useRouter } from "next/navigation";
 
 export default function SuccessPage({ params }) {
+  const router = useRouter();
   const unwrappedParams = use(params);
   const shortUrl = unwrappedParams.short_url;
 
   return (
     <ThemeProvider theme={theme}>
       <Box sx={styles.box}>
-        <Box sx={styles.shortUrlBox}>
-          <TextField
-            value={shortUrl}
-            variant="outlined"
-            size="small"
-            inputProps={{
-              readOnly: true,
-            }}
-            fullWidth
-          />
+        <TextField
+          value={shortUrl}
+          variant="outlined"
+          size="small"
+          inputProps={{
+            readOnly: true,
+          }}
+          sx={styles.textField}
+          fullWidth
+        />
+        <Box sx={styles.buttonBox}>
           <Button
             variant="outlined"
-            size="small"
             onClick={() => navigator.clipboard.writeText(shortUrl)}
           >
             Copy
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => router.push(`/${shortUrl}`)}
+          >
+            Open Link
           </Button>
         </Box>
       </Box>
@@ -44,13 +52,16 @@ const styles = {
     justifyContent: "center",
     padding: 2,
   },
-  shortUrlBox: {
+  textField: {
+    maxWidth: 300,
+  },
+  buttonBox: {
     display: "flex",
-    alignItems: "top",
-    gap: 1,
+    gap: 2,
     mt: 2,
     width: "100%",
     maxWidth: 300,
     height: 40,
+    justifyContent: "end",
   },
 };
