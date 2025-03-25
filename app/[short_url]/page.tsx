1a03308f9b7190f3e2aa9e26835a11701a78873b
ilaumjd <ilaumjd@gmail.com>
2025-03-25 21:19:58 +0700

    add redirect page

---
+++ app/[short_url]/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { use, useEffect } from "react";
+import { useFetchUrl } from "./hook";
+
+type RedirectProps = {
+  params: Promise<{ short_url: string }>;
+};
+
+export default function Redirect(props: RedirectProps) {
+  const params = use(props.params);
+  const { fetchUrl } = useFetchUrl(params.short_url);
+
+  useEffect(() => {
+    fetchUrl().then((result) => {
+      if (result.success) {
+        window.location.href = `https://${result.data?.defaultUrl}`;
+      }
+    });
+  }, [fetchUrl]);
+
+  return <div>Redirecting...</div>;
+}
--- app/success/[short_url]/page.tsx
+++ app/success/[short_url]/page.tsx
@@ -6,18 +6,18 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FetchUrlResponse, useFetchUrl } from "@/app/[short_url]/hook";
 
-export type SuccessPageProps = {
+type SuccessProps = {
   params: Promise<{ short_url: string }>;
 };
 
-export default function SuccessPage(props: SuccessPageProps) {
+export default function Success(props: SuccessProps) {
   const params = use(props.params);
 
   const { fetchUrl } = useFetchUrl(params.short_url);
   const [urlResult, setUrlResult] = useState<FetchUrlResponse>({});
   const defaultUrl = `https://${urlResult.data?.defaultUrl}`;
-  const shortUrlHash = urlResult.data?.shortUrl;
-  const shortUrl = `https://pende.cc/${urlResult.data?.shortUrl}`;
+  const shortUrlHash = `/${urlResult.data?.shortUrl}`;
+  const shortUrl = `https://pende.cc${shortUrlHash}`;
 
   useEffect(() => {
     fetchUrl().then((result) => {
@@ -37,9 +37,14 @@ export default function SuccessPage(props: SuccessPageProps) {
       <Input type="text" readOnly value={shortUrl} className="mt-4" />
 
       <div className="flex gap-2 mt-4">
-        <Button className="">Copy</Button>
+        <Button
+          className=""
+          onClick={() => navigator.clipboard.writeText(shortUrl)}
+        >
+          Copy
+        </Button>
         <Button className="">
-          <Link href={shortUrlHash ?? ""} target="_blank">
+          <Link href={shortUrlHash} target="_blank">
             Open Link
           </Link>
         </Button>
