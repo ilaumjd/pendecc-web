34091ecb0eea30c906cd76ccb5325485c52fe629
ilaumjd <ilaumjd@gmail.com>
2025-03-27 20:04:22 +0700

    add redirecting animation + not found

--- app/[short_url]/page.tsx
+++ app/[short_url]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { use, useEffect } from "react";
+import { use, useEffect, useState } from "react";
 import { useFetchUrl } from "./hook";
+import { Label } from "@/components/ui/label";
 
 type RedirectProps = {
   params: Promise<{ short_url: string }>;
@@ -10,13 +11,35 @@ export default function Redirect(props: RedirectProps) {
   const params = use(props.params);
   const { fetchUrl } = useFetchUrl(params.short_url);
 
+  const [notFound, setNotFound] = useState(false);
   useEffect(() => {
     fetchUrl().then((result) => {
       if (result.success) {
         window.location.href = `${result.data?.defaultUrl}`;
+      } else {
+        setNotFound(true);
       }
     });
   }, [fetchUrl]);
 
-  return <div>Redirecting...</div>;
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 400);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center text-2xl">
+      {notFound ? (
+        <Label>Not Found</Label>
+      ) : (
+        <Label>
+          Redirecting
+          {Array.from({ length: counter % 4 }, () => ".").join("")}
+        </Label>
+      )}
+    </div>
+  );
 }
