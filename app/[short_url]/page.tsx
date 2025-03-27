a08e488623a44d9c53246d38ef691342f6145c2a
ilaumjd <ilaumjd@gmail.com>
2025-03-27 20:25:29 +0700

    better not found

--- app/[short_url]/page.tsx
+++ app/[short_url]/page.tsx
@@ -2,6 +2,8 @@
 import { use, useEffect, useState } from "react";
 import { useFetchUrl } from "./hook";
 import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 type RedirectProps = {
   params: Promise<{ short_url: string }>;
@@ -31,11 +33,18 @@ export default function Redirect(props: RedirectProps) {
   }, []);
 
   return (
-    <div className="flex items-center justify-center text-2xl">
+    <div className="flex items-center justify-center">
       {notFound ? (
-        <Label>Not Found</Label>
+        <div className="flex flex-col items-center justify-center text-center">
+          <Label className="text-2xl">Not Found</Label>
+          <Label className="mt-4">{`The URL https://pende.cc/${params.short_url} is not found.`}</Label>
+          <Label className="mt-2">You can claim it by yourself 😁</Label>
+          <Button className="mt-4">
+            <Link href="/">← Home</Link>
+          </Button>
+        </div>
       ) : (
-        <Label>
+        <Label className="text-2xl">
           Redirecting
           {Array.from({ length: counter % 4 }, () => ".").join("")}
         </Label>
